31f5df6385be1084c9e982d1aa1ce057dec4b208
thepaperpilot <thepaperpilot@gmail.com>
2018-01-10 00:51:16 -0600

    Version bump

--- server/main.js
+++ server/main.js
@@ -11,7 +11,7 @@ const ss = require('socket.io-stream')
 var port = process.env.babblePort || 8080 // Will read port from environment variable "babblePort" or default to 8080
 var assetsPath = path.join(__dirname, 'assets')
 var logLevel = 2 // 0 = No messages, 1 = Connect/Disconnect messages, 2 = Also include puppet changes, 3 = All known commands
-var clientVersion = "~0.7.0"	// Clients will need to match this version/range
+var clientVersion = "~0.8.0"	// Clients will need to match this version/range
 
 // Variables
 var server
