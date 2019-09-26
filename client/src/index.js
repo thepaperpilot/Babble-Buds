dc0b887d4d47a657e0419364810e1dba1d61f7fb
thepaperpilot <thepaperpilot@gmail.com>
2019-09-25 23:46:31 -0500

    Added redux-thunk middleware and redux-batched-subscribe enhancer

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
+import ReactDOM, { unstable_batchedUpdates } from 'react-dom'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { batchedSubscribe } from 'redux-batched-subscribe'
 import App from './App'
 import reducer from './reducers/index'
 
 import './index.css'
 
-const store = createStore(reducer, applyMiddleware(logger))
+const enhancer = compose(
+    applyMiddleware(logger, thunk),
+    batchedSubscribe(unstable_batchedUpdates)
+)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
     <Provider store={store}>
