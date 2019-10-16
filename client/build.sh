08daae90a9ce9bbfffeefcb69f1639cc8f685d5a
thepaperpilot <thepaperpilot@gmail.com>
2019-10-16 11:12:18 -0500

    Fixed empty and sample projects not being copied over to the build folder during packaging

--- client/build.sh
+++ client/build.sh
@@ -4,6 +4,8 @@ mv build/.public build/public
 mv build/public/package.json build
 cp src/electron-starter.js build/public
 cp -r src/main-process build/public
-cp -r src/data build/public
 cp -r src/background build/public
+cp -r src/data build/public
+cp -r src/empty-project build/public
+cp -r src/sample-project build/public
 cp -r node_modules build
