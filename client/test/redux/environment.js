25db98d9e90e9d5bf4317b56a9f611c304fec8c3
thepaperpilot <thepaperpilot@gmail.com>
2019-10-25 05:15:35 -0500

    Added tests for redux/environment

---
+++ client/test/redux/environment.js
@@ -0,0 +1,74 @@
+import chai, { expect } from 'chai'
+import chaiRedux from 'chai-redux'
+import thunk from 'redux-thunk'
+import { combineReducers } from 'redux'
+import logFailedStore from '../util/logFailedStore'
+import fakeReducer from '../util/fakeReducer'
+import environment, { setEnvironment, setDefaultEnvironment } from '../../src/redux/environment'
+
+chai.use(chaiRedux)
+
+let store
+const reducer = combineReducers({
+    environment,
+    defaults: fakeReducer
+})
+const initialState = {
+    environment: null,
+    defaults: {
+        environment: {
+            name: 'fake default environment',
+            foo: 'bar'
+        }
+    }
+}
+const middleware = thunk
+
+describe('redux/environment', function () {
+    beforeEach(() => {
+        store = chai.createReduxStore({ reducer, middleware, initialState })
+    })
+
+    afterEach(logFailedStore(() => store.getState()))
+
+    it("should set environment", () => {
+        store.dispatch(setEnvironment('fake setter', 'fake environment id', {
+            name: 'fake environment',
+            foo: 'bar'
+        }))
+        expect(store).to.have.state.like({
+            environment: {
+                name: 'fake environment',
+                foo: 'bar',
+                setter: 'fake setter',
+                environmentId: 'fake environment id'
+            }
+        })
+    })
+
+    it("should set default environment", () => {
+        store.dispatch(setDefaultEnvironment())
+        expect(store).to.have.state.like({
+            environment: {
+                name: 'fake default environment',
+                foo: 'bar'
+            }
+        })
+    })
+
+    it("should ignore reserved properties in environment", () => {
+        store.dispatch(setEnvironment('fake setter', 'fake environment id', {
+            name: 'fake environment',
+            foo: 'bar',
+            setter: 'reserved'
+        }))
+        expect(store).to.have.state.like({
+            environment: {
+                name: 'fake environment',
+                foo: 'bar',
+                setter: 'fake setter',
+                environmentId: 'fake environment id'
+            }
+        })
+    })
+})
