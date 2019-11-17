d252083c43386d11e71214c9097a5c20eb2579de
thepaperpilot <thepaperpilot@gmail.com>
2019-11-17 00:13:32 -0600

    Added tests for redux/project/dirtyCharacters

---
+++ client/test/redux/project/dirtyCharacters.js
@@ -0,0 +1,61 @@
+import chai, { expect } from 'chai'
+import chaiRedux from 'chai-redux'
+import thunk from 'redux-thunk'
+import { combineReducers } from 'redux'
+import mock from 'mock-require'
+import dirtyCharacters, { addCharacters, clearCharacters } from '../../../src/redux/project/dirtyCharacters'
+
+chai.use(chaiRedux)
+
+const reducer = combineReducers({
+    project: combineReducers({
+        dirtyCharacters
+    })
+})
+
+const middleware = thunk
+
+describe('redux/project/dirtyCharacters', function () {
+    after(mock.stopAll)
+
+    it("should add characters", () => {
+        const initialState = {
+            project: {
+                dirtyCharacters: []
+            }
+        }
+        const store = chai.createReduxStore({ reducer, middleware, initialState })
+
+        const characterThumbnails = [
+            'test',
+            'test2',
+            'test3'
+        ]
+        store.dispatch(addCharacters(characterThumbnails))
+        expect(store).to.have.state.like({
+            project: {
+                ...store.getState().project,
+                dirtyCharacters: characterThumbnails
+            }
+        })
+    })
+
+    it('should clear characters', () => {
+        const initialState = {
+            project: {
+                dirtyCharacters: [
+                    'test'
+                ]
+            }
+        }
+        const store = chai.createReduxStore({ reducer, middleware, initialState })
+
+        store.dispatch(clearCharacters())
+        expect(store).to.have.state.like({
+            project: {
+                ...store.getState().project,
+                dirtyCharacters: []
+            }
+        })
+    })
+})
