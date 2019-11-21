6a65c2477ecfd90d01337320b587290c5673829a
thepaperpilot <thepaperpilot@gmail.com>
2019-11-21 00:34:00 -0600

    Added tests for redux/project/settings/hotbar

--- client/src/redux/project/settings/hotbar.js
+++ client/src/redux/project/settings/hotbar.js
@@ -2,6 +2,25 @@ import util from '../../util.js'
 import { getActor } from '../../actors'
 import { changePuppet } from '../../controller'
 
+// Utility Functions
+function updateActors(dispatch, state, actor, slotIndex, newValue) {
+    const prevHotbar = state.project.settings.hotbar
+    const characters = state.project.characters
+
+    // if we didn't change this slot, we don't need to change the actor
+    if (prevHotbar[slotIndex] === newValue)
+        return
+
+    const matches = prevHotbar.filter(puppet => puppet == actor.puppetId)
+    // If its a non-one amount, we don't need to change the actor
+    if (matches.length !== 1)
+        return
+
+    // If we changed the one hotbar slot this actor matches,
+    // change the actor to the new puppet
+    dispatch(changePuppet(actor.id, newValue, characters[newValue]))
+}
+
 // Action Types
 const SET_ALL = 'project/settings/hotbars/SET_ALL'
 const SET = 'project/settings/hotbars/SET'
@@ -11,25 +30,12 @@ export function setHotbar(hotbar) {
     return (dispatch, getState) => {
         const state = getState()
         const controller = state.controller
-        const old = state.project.settings.hotbar
-        const characters = state.project.characters
         
         // Change any actors we're controlling that'd be affected
         controller.actors.forEach(id => {
             const actor = getActor(state, id)
-            // Find any hotbar slots set to the puppet this actor currently is
-            const matches = old.filter(puppet => actor.puppetId)
-            // If its a non-one amount, we don't need to change the actor
-            if (matches.length !== 1)
-                return
-            // if we didn't change this slot, we don't need to change the actor
-            const index = old.findIndex(puppet => actor.puppetId)
-            if (old[index] === hotbar[index])
-                return
-
-            // If we changed the one hotbar slot this actor matches,
-            // change the actor to the new puppet
-            dispatch(changePuppet(id, hotbar[index], characters[hotbar[index]]))
+            hotbar.forEach((newValue, index) =>
+                updateActors(dispatch, state, actor, index, newValue))
         })
 
         dispatch({ type: SET_ALL, hotbar })
@@ -40,25 +46,12 @@ export function setSlot(slot, puppet) {
     return (dispatch, getState) => {
         const state = getState()
         const controller = state.controller
-        const old = state.project.settings.hotbar
-        const characters = state.project.characters
 
         // If we're not clearing the slot, we may want to change our puppet
         if (puppet !== 0)
             controller.actors.forEach(id => {
                 const actor = getActor(state, id)
-                // Find any hotbar slots set to the puppet this actor currently is
-                const matches = old.filter(puppet => actor.puppetId)
-                // If its a non-one amount, we don't need to change the actor
-                if (matches.length !== 1)
-                    return
-                // if we didn't change this slot, we don't need to change the actor
-                if (old[slot] === puppet)
-                    return
-
-                // If we changed the one hotbar slot this actor matches,
-                // change the actor to the new puppet
-                dispatch(changePuppet(id, puppet, characters[puppet]))
+                updateActors(dispatch, state, actor, slot, puppet)
             })
 
         dispatch({ type: SET, index: slot, id: puppet })
---
+++ client/test/redux/project/settings/hotbar.js
@@ -0,0 +1,111 @@
+import chai, { expect } from 'chai'
+import chaiRedux from 'chai-redux'
+import thunk from 'redux-thunk'
+import { combineReducers } from 'redux'
+import mock from 'mock-require'
+import fakeReducer from '../../../util/fakeReducer'
+import fakeActions from '../../../util/fakeActions'
+
+chai.use(chaiRedux)
+
+let hotbar, setHotbar, setSlot
+let reducer
+
+const defaultHotbar = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+const updatedHotbar = [2, 0, 0, 0, 0, 0, 0, 0, 0]
+
+const middleware = thunk
+
+describe('redux/project/settings/hotbar', function () {
+    before(() => {
+        mock('../../../../src/redux/controller', fakeActions('changePuppet'))
+
+        const h = mock.reRequire('../../../../src/redux/project/settings/hotbar')
+        hotbar = h.default
+        setHotbar = h.setHotbar
+        setSlot = h.setSlot
+
+        reducer = combineReducers({
+            project: combineReducers({
+                settings: combineReducers({
+                    hotbar
+                }),
+                characters: fakeReducer
+            }),
+            controller: fakeReducer,
+            actors: fakeReducer
+        })
+    })
+
+    after(mock.stopAll)
+
+    it("should set hotbar", () => {
+        const initialState = {
+            project: {
+                settings: {
+                    hotbar: defaultHotbar
+                },
+                characters: {
+                    2: "test puppet"
+                }
+            },
+            controller: {
+                actors: [0]
+            },
+            actors: [
+                { id: 0, puppetId: 1 }
+            ]
+        }
+        const store = chai.createReduxStore({ reducer, middleware, initialState })
+
+        store.dispatch(setHotbar(updatedHotbar))
+        expect(store).to.have.state.like({
+            project: {
+                ...store.getState().project,
+                settings: {
+                    hotbar: updatedHotbar
+                }
+            }
+        })
+        .and.dispatched({ f: 'changePuppet', args: [
+            0,
+            2,
+            "test puppet"
+        ]})
+    })
+
+    it('should set slot', () => {
+        const initialState = {
+            project: {
+                settings: {
+                    hotbar: defaultHotbar
+                },
+                characters: {
+                    2: "test puppet"
+                }
+            },
+            controller: {
+                actors: [0]
+            },
+            actors: [
+                { id: 0, puppetId: 1 }
+            ]
+        }
+        const store = chai.createReduxStore({ reducer, middleware, initialState })
+
+        store.dispatch(setSlot(0, 2))
+        expect(store).to.have.state.like({
+            project: {
+                ...store.getState().project,
+                settings: {
+                    hotbar: updatedHotbar
+                }
+            }
+        })
+        .and.dispatched({ f: 'changePuppet', args: [
+            0,
+            2,
+            "test puppet"
+        ]})
+    })
+})
