9c204d101376c944bc4db4b656959910a10084fc
thepaperpilot <thepaperpilot@gmail.com>
2019-12-18 21:43:33 -0600

    Fixed server crash when changing nickname

--- server/main.js
+++ server/main.js
@@ -117,7 +117,7 @@ server.sockets.on('connection', function(socket) {
 	socket.on('change nickname', (nickname) => {
 		let room = rooms[socket.room]
 		if (!socket.room || !room) return
-		if (logLevel >= 2) console.log(`${room.users[id].nickname} (${socket.id}) changed their nickname to ${nickname}`)
+		if (logLevel >= 2) console.log(`${room.users[socket.id].nickname} (${socket.id}) changed their nickname to ${nickname}`)
         room.users[socket.id].nickname = nickname
 		socket.broadcast.to(socket.room).emit('change nickname', socket.id, nickname)
 	})
