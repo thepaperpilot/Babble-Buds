8c4153f0c67304f865829e62a5c6623767e3a060
thepaperpilot <thepaperpilot@gmail.com>
2020-09-06 22:16:17 -0500

    Fixed build pipeline issues on windows

--- client/build.bat
+++ client/build.bat
@@ -1,6 +1,7 @@
 move popout-build\index.html build\popout-index.html
-robocopy popout-build\static\js build\static\js
-del /q popout-build
+robocopy popout-build\static\js build\static\js > NUL
+del /q/s popout-build > NUL
+rmdir /q/s popout-build
 setlocal
 cd build && call npm install
 endlocal
@@ -10,11 +11,11 @@ move build\icons build\.public\
 move build\static build\.public\
 rename build\.public public
 move build\public\package.json build\
-robocopy src build\public\ electron-starter.js
-robocopy src\main-process build\public\main-process /E
-robocopy src\background build\public\background /E
-robocopy src\data build\public\data /E
-robocopy src\empty-project build\public\empty-project /E
-robocopy src\sample-project build\public\sample-project /E
-robocopy node_modules build\node_modules /E
+robocopy src build\public\ electron-starter.js > NUL
+robocopy src\main-process build\public\main-process /E > NUL
+robocopy src\background build\public\background /E > NUL
+robocopy src\data build\public\data /E > NUL
+robocopy src\empty-project build\public\empty-project /E > NUL
+robocopy src\sample-project build\public\sample-project /E > NUL
+robocopy node_modules build\node_modules /E > NUL
 exit 0
